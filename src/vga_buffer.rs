3034feed7aaeb18a83117111689872f987a49e3d
Justin Alan Ryan <justin.alan.ryan@gmail.com>
2019-01-05 16:40:29 +0000

    pull core::fmt usage as a placeholder to find out later why this doesn't loop.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,4 @@
 use volatile::Volatile;
-use core::fmt;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -89,13 +88,6 @@ impl Writer {
     fn new_line(&mut self) {/* TODO */}
 }
 
-impl fmt::Write for Writer {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
-        self.write_string(s);
-        Ok(())
-    }
-}
-
 pub fn print_something() {
     use core::fmt::Write;
     let mut writer = Writer {
@@ -106,5 +98,4 @@ pub fn print_something() {
 
     writer.write_byte(b'H');
     writer.write_string("ello ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
