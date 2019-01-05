fcccb4277d48741fc5a2e1d1af45533a72299a74
Justin Alan Ryan <justin.alan.ryan@gmail.com>
2019-01-05 22:26:48 +0000

    add test harness, first test, and conditionals to allow tests to run.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-#![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(unused_imports))]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
   println!("{}", _info);
   loop{}
 }
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   println!("Hello World{}", "!");
