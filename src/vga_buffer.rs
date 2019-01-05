e4bc3be55d622c98c5949754df13453b1464a784
Justin Alan Ryan <justin.alan.ryan@gmail.com>
2019-01-05 17:48:24 +0000

    add newline func, global writer.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,5 @@
 use volatile::Volatile;
+use lazy_static::lazy_static;
 use core::fmt;
 
 #[allow(dead_code)]
@@ -86,7 +87,26 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 impl fmt::Write for Writer {
@@ -94,6 +114,14 @@ impl fmt::Write for Writer {
         self.write_string(s);
         Ok(())
     }
+}
+
+lazy_static! {
+  pub static ref WRITER: Writer = Writer {
+    column_position: 0,
+    color_code: ColorCode::new(Color::LightGreen, Color::Black),
+    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+  };
 }
 
 pub fn print_something() {
@@ -108,3 +136,4 @@ pub fn print_something() {
     writer.write_string("ello ");
     write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
+
