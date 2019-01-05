8970418f4b732d4428eac844eaf0813cb8cf371e
Justin Alan Ryan <justin.alan.ryan@gmail.com>
2019-01-05 23:34:01 +0000

    add qemu exit code.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ use core::panic::PanicInfo;
 mod vga_buffer;
 mod serial;
 
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
+}
+
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -18,6 +25,8 @@ fn panic(_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
   serial_println!("Hello Host{}", "!");
   println!("Hello World{}", "!");
+
+  unsafe { exit_qemu(); }
 
   loop {}
 }
