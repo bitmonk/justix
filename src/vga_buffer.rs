a6cfd01c9b5298c4a075ac5cf68f01316c55bd45
Justin Alan Ryan <justin.alan.ryan@gmail.com>
2019-01-06 17:59:00 +0000

    move static writer to top to match upstream.

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -3,6 +3,14 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use core::fmt;
 
+lazy_static! {
+  pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+    column_position: 0,
+    color_code: ColorCode::new(Color::LightGreen, Color::Black),
+    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+  });
+}
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -115,14 +123,6 @@ impl fmt::Write for Writer {
         self.write_string(s);
         Ok(())
     }
-}
-
-lazy_static! {
-  pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
-    column_position: 0,
-    color_code: ColorCode::new(Color::LightGreen, Color::Black),
-    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-  });
 }
 
 #[macro_export]
