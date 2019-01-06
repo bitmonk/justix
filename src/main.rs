e7ff3a50522ee5b230f98297a8deabaf081dc248
Justin Alan Ryan <justin.alan.ryan@gmail.com>
2019-01-06 21:54:08 +0000

    move exit_qemu to library, don't use in main bc it's for testing.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,26 @@
-#![cfg_attr(not(test), no_std)]
+#![no_std]
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(unused_imports))]
 
 use core::panic::PanicInfo;
-mod vga_buffer;
-mod serial;
-
-pub unsafe fn exit_qemu() {
-    use x86_64::instructions::port::Port;
-
-    let mut port = Port::<u32>::new(0xf4);
-    port.write(0);
-}
+use justix::println;
+use justix::vga_buffer;
+use justix::serial_println;
+use justix::exit_qemu;
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-  println!("{}", _info);
-  loop{}
+    println!("{}", _info);
+    loop{}
 }
 
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
   vga_buffer::print_something();
-//  serial_println!("Hello Host{}", "!");
-//  println!("Hello World{}", "!");
-
-//   unsafe { exit_qemu(); }
+  serial_println!("Hello Host{}", "!");
+  println!("Hello World{}", "!");
 
   loop {}
 }
