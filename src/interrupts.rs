d0d50b17810ed729b1a876b910351ff8ed54c6e5
Justin Alan Ryan <justin.alan.ryan@gmail.com>
2019-01-13 19:38:44 +0000

    change to lazy_static for interrupt descriptor table.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,13 +1,17 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame};
+use lazy_static::lazy_static;
 use crate::println;
 
-static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 pub fn init_idt() {
-    unsafe {
-        IDT.breakpoint.set_handler_fn(breakpoint_handler);
-        IDT.load();
-    }
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
